eb8f53ca01c56ff845d720bcebbd8aad5d9240a3
krekus <vkrerer@gmail.com>
2023-03-25 17:59:51 +0300

    module9-task1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import {picturesDataList} from './data.js';
 import {renderThumbnails} from './thumbnails.js';
-import './open-modal.js';
-
+import {fileField, openForm} from './form.js';
+import './modal.js';
 
 renderThumbnails(picturesDataList);
+
+fileField.addEventListener('change', openForm);
