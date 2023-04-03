ce730314a7cdd609d529efd3702c34bd3a5a9994
Krekus <58678923+iKrekus@users.noreply.github.com>
2023-04-03 16:32:42 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {renderThumbnails} from './thumbnails.js';
 import {fileField, openForm, closeForm, setOnFormSubmit, showErrorMesaage, showSuccessMessage} from './form.js';
 import {getData, sendData} from './api.js';
 import {showAlert, debounce} from './utils.js';
-import {sortInit, getSortedPictures} from './sort.js';
+import {sortInit} from './sort.js';
 import './modal.js';
 
 
