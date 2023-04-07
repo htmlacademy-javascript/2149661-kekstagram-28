e9339350b7203f6e6963a08247e8b15e26abbea9
krekus <vkrerer@gmail.com>
2023-04-07 21:54:44 +0300

    to master-exam-change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
-import {picturesDataList} from './data.js';
 import {renderThumbnails} from './thumbnails.js';
-import './open-modal.js';
+import {onClickUploadPoto, closeForm, setOnFormSubmit, showErrorMesaage, showSuccessMessage} from './form.js';
+import {getData, sendData} from './api.js';
+import {showAlert, debounce} from './utils.js';
+import {sortInit} from './sort.js';
+import {openModalListeners} from './modal.js';
+
+onClickUploadPoto();
+
+setOnFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    closeForm();
+    showSuccessMessage();
+  } catch {
+    showErrorMesaage();
+  }
+});
 
 
-renderThumbnails(picturesDataList);
+try {
+  const data = await getData();
+  const debouncedThumbnails = debounce(renderThumbnails);
+  sortInit(data , debouncedThumbnails);
+  renderThumbnails(data);
 
+} catch (err) {
+  showAlert(err.message);
+}
+
+openModalListeners();
